f95ecbe55320f615ab15f1c67f7055a6a6bb645e
Ashroom66 <ashroom09@gmail.com>
2022-08-19 17:12:42 +0900

    球体のレンダリング

--- main.cpp
+++ main.cpp
@@ -4,11 +4,22 @@
 #include <iostream>
 using namespace std;
 
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = r.origin() - center;  // レイのA-C, レイ原点と球座標をつなぐ
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b-4*a*c;
+    return (discriminant > 0);
+}
+
 color ray_color(const ray& r) {
+    if (hit_sphere(point3(0, 0, -1), 0.2, r)) {return color(1, 0, 0);}  // 球体描画
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5*(unit_direction.y() + 1.0);
     return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
 }
+
 
 int main() {
     const auto aspect_ratio = 16.0 / 9.0;
