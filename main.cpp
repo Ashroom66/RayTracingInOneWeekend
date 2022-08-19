3c99dac1ca3ded48a1c0444ced7eee1133573f91
Ashroom66 <ashroom09@gmail.com>
2022-08-19 17:23:21 +0900

    球の表示

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ bool hit_sphere(const point3& center, double radius, const ray& r) {
 }
 
 color ray_color(const ray& r) {
-    if (hit_sphere(point3(0, 0, -1), 0.2, r)) {return color(1, 0, 0);}  // 球体描画
+    if (hit_sphere(point3(0, 0, -1), 0.5, r)) {return color(1, 0, 0);}  // 球体描画
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5*(unit_direction.y() + 1.0);
     return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
